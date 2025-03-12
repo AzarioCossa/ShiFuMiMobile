e751d28de7c56cf2c32cf3b13d2da9f0ae94bfca
Evan Gerbeaud <gerbeaudevan@gmail.com>
2025-03-12 22:41:48 +0100

    add a sentence about the result of the match

--- app/src/main/java/com/example/shifumi_mobile/controllers/GameController.kt
+++ app/src/main/java/com/example/shifumi_mobile/controllers/GameController.kt
@@ -6,14 +6,14 @@ import kotlin.random.Random
 class GameController {
     private var lastResult: String = ""
 
-    fun playGame(): List<Int> {
+    fun playGame(): Triple<Int, Int, String> {
         val choices = listOf("Pierre", "Papier", "Ciseaux")
         val playerChoice = choices.random()
         val computerChoice = choices.random()
 
         
         lastResult = "L'ordinateur a choisi: $computerChoice"
-
+        val winnerMessage = determineWinner(playerChoice, computerChoice)
 
         fun getImage(choice: String): Int {
             return when (choice) {
@@ -24,11 +24,27 @@ class GameController {
             }
         }
 
-        return listOf(getImage(playerChoice), getImage(computerChoice))
+        return Triple(getImage(playerChoice), getImage(computerChoice), winnerMessage)
     }
 
     fun getLastResult(): String {
         return lastResult
     }
+
+    fun determineWinner(playerChoice: String, computerChoice: String): String {
+        return if (playerChoice == computerChoice) {
+            "Match nul !"
+        } else if (
+            (playerChoice == "Pierre" && computerChoice == "Ciseaux") ||
+            (playerChoice == "Ciseaux" && computerChoice == "Papier") ||
+            (playerChoice == "Papier" && computerChoice == "Pierre")
+        ) {
+            "Vous avez gagné !"
+        } else {
+            "L'ordinateur a gagné !"
+        }
+    }
+
+
 }
 
--- app/src/main/java/com/example/shifumi_mobile/ui/PlayScreen.kt
+++ app/src/main/java/com/example/shifumi_mobile/ui/PlayScreen.kt
@@ -13,7 +13,6 @@ import androidx.navigation.NavHostController
 import com.example.shifumi_mobile.controllers.GameController
 import com.example.shifumi_mobile.R
 
-
 @Composable
 fun PlayScreen(navController: NavHostController, isShaken: MutableState<Boolean>) {
     var result by remember { mutableStateOf("") }
@@ -23,26 +22,15 @@ fun PlayScreen(navController: NavHostController, isShaken: MutableState<Boolean>
 
     LaunchedEffect(isShaken.value) {
         if (isShaken.value) {
-            val images = gameController.playGame() // Récupère les 2 images (joueur + ordinateur)
-            playerImageResId = images[0] // Image du joueur
-            computerImageResId = images[1] // Image de l'ordinateur
-
-            result = "Résultat: ${gameController.getLastResult()}"
+            val (playerImage, computerImage, winnerMessage) = gameController.playGame()
+            playerImageResId = playerImage
+            computerImageResId = computerImage
+            result = winnerMessage
             isShaken.value = false
         }
     }
 
     ImageBackground(0.5f)
-
-    Button(
-        onClick = { navController.navigate("home") },
-        colors = ButtonDefaults.buttonColors(containerColor = MaterialTheme.colorScheme.primary),
-        modifier = Modifier
-            .padding(10.dp)
-            .padding(top = 26.dp),
-    ) {
-        Text(text = "Retour à l'accueil")
-    }
 
     Column(
         modifier = Modifier
@@ -51,46 +39,44 @@ fun PlayScreen(navController: NavHostController, isShaken: MutableState<Boolean>
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
+        Button(
+            onClick = { navController.navigate("home") },
+            colors = ButtonDefaults.buttonColors(containerColor = MaterialTheme.colorScheme.primary),
+            modifier = Modifier.padding(bottom = 20.dp)
+        ) {
+            Text(text = "Retour à l'accueil")
+        }
 
-
+        //Choix ordinateur
         Column(
             horizontalAlignment = Alignment.CenterHorizontally,
             modifier = Modifier.padding(bottom = 40.dp)
         ) {
-            Text(
-                text = "Ordinateur",
-                fontSize = 25.sp
+            Text(text = "Ordinateur", fontSize = 25.sp)
+            Image(
+                painter = painterResource(id = computerImageResId),
+                contentDescription = "Choix de l'ordinateur",
+                modifier = Modifier.size(200.dp)
             )
-            Box(modifier = Modifier.size(200.dp)) {
-                Image(
-                    painter = painterResource(id = computerImageResId),
-                    contentDescription = "Choix de l'ordinateur",
-                    modifier = Modifier.size(200.dp)
-                )
-            }
         }
 
+        //Resultat
         Text(
             text = result.ifEmpty { "Secouez le portable pour jouer" },
-            fontSize = 18.sp
+            fontSize = 22.sp,
+            modifier = Modifier.padding(vertical = 20.dp)
         )
 
+        //Choix du joueur
         Column(
-            horizontalAlignment = Alignment.CenterHorizontally,
-            modifier = Modifier.padding(top = 40.dp)
+            horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            Text(
-                text = "Vous",
-                fontSize = 25.sp
+            Text(text = "Vous", fontSize = 25.sp)
+            Image(
+                painter = painterResource(id = playerImageResId),
+                contentDescription = "Votre choix",
+                modifier = Modifier.size(200.dp)
             )
-            Box(modifier = Modifier.size(200.dp)) {
-                Image(
-                    painter = painterResource(id = playerImageResId),
-                    contentDescription = "Votre choix",
-                    modifier = Modifier.size(200.dp)
-                )
-            }
         }
     }
 }
-
