f5edc6b709b764143778e16b00f751d0e3edbc76
AzarioCossa <cossaazario@gmail.com>
2025-04-06 21:33:18 +0200

    add style correction

--- app/src/main/java/com/example/shifumi_mobile/ui/WifiScreen.kt
+++ app/src/main/java/com/example/shifumi_mobile/ui/WifiScreen.kt
@@ -14,6 +14,7 @@ import com.example.shifumi_mobile.wifi.WifiManager
 
 @Composable
 fun WifiScreen(wifiManager: WifiManager) {
+    ImageBackground(0.5f)
     val messages = remember { mutableStateListOf<String>() }
     var messageText by remember { mutableStateOf(TextFieldValue("")) }
     wifiManager.checkConnectionInfo()
