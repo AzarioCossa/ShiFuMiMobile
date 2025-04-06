bd4c2ec04dff73fb38ededb664f463933eb8312b
AzarioCossa <cossaazario@gmail.com>
2025-04-06 21:22:41 +0200

    refacto : language adaptation

--- app/src/main/java/com/example/shifumi_mobile/wifi/WifiManager.kt
+++ app/src/main/java/com/example/shifumi_mobile/wifi/WifiManager.kt
@@ -15,12 +15,10 @@ class WifiManager(private val context: Context) {
     public var outputStream: OutputStream? = null
     private var inputStream: BufferedReader? = null
     private var isServerRunning = false
-    // Callback para tratar mensagens recebidas
     var onMessageReceived: ((String) -> Unit)? = null
 
     private val executor: ExecutorService = Executors.newSingleThreadExecutor()
 
-    // Wi-Fi Direct Manager
     private val manager: WifiP2pManager = context.getSystemService(Context.WIFI_P2P_SERVICE) as WifiP2pManager
     private val channel: WifiP2pManager.Channel = manager.initialize(context, context.mainLooper, null)
 
@@ -28,16 +26,15 @@ class WifiManager(private val context: Context) {
         manager.requestConnectionInfo(channel) { info ->
             if (info.groupFormed) {
                 if (info.isGroupOwner) {
-                    // O dispositivo é o servidor (Group Owner)
-                    Log.d("WifiManager", "Sou o servidor! Iniciando o servidor.")
-                    startServer()  // Inicia o servidor
+                    Log.d("WifiManager", "I am the server! Starting server.")
+                    startServer()
                 } else {
                     // O dispositivo é o cliente
-                    Log.d("WifiManager", "Sou o cliente! Conectando ao servidor.")
+                    Log.d("WifiManager", "I am a client! Connecting to the server.")
                     connectToServer(info.groupOwnerAddress.hostAddress)  // Conecta ao servidor
                 }
             } else {
-                Log.e("WifiManager", "Não há grupo formado.")
+                Log.e("WifiManager", "No existing group.")
             }
         }
     }
@@ -48,43 +45,40 @@ class WifiManager(private val context: Context) {
             executor.execute {
                 try {
                     serverSocket = ServerSocket(port)
-                    Log.d("WifiManager", "Aguardando conexão...")
+                    Log.d("WifiManager", "Awaiting connection...")
                     socket = serverSocket!!.accept()
-                    Log.d("WifiManager", "Cliente conectado: ${socket!!.inetAddress.hostAddress}")
+                    Log.d("WifiManager", "Client connected: ${socket!!.inetAddress.hostAddress}")
                     setupStreams()
                     listenForMessages()
                 } catch (e: IOException) {
-                    Log.e("WifiManager", "Erro ao iniciar servidor: ${e.message}")
+                    Log.e("WifiManager", "Error when starting server: ${e.message}")
                     isServerRunning = false
                 }
             }
         } else {
-            Log.d("WifiManager", "Servidor já está em execução.")
+            Log.d("WifiManager", "Server already in execution.")
         }
     }
 
     fun connectToServer(ip: String, port: Int = 8888) {
-        // Verifica se o socket já está conectado
         if (socket?.isConnected == true) {
-            Log.d("WifiManager", "Já está conectado ao servidor: ${socket?.inetAddress?.hostAddress}")
-            return  // Não faz nada se já estiver conectado
+            Log.d("WifiManager", "Already connected with server: ${socket?.inetAddress?.hostAddress}")
+            return
         }
 
-        // Caso o socket não esteja conectado, tenta conectar
         executor.execute {
             try {
-                // Cria o socket e tenta conectar
-                socket = Socket(ip, port)
-                Log.d("WifiManager", "Conectado ao servidor: $ip")
 
-                // Configura os fluxos de entrada e saída
+                socket = Socket(ip, port)
+                Log.d("WifiManager", "Connected to server: $ip")
+
                 setupStreams()
 
                 // Inicia a escuta de mensagens
                 //listenForMessages()
 
             } catch (e: IOException) {
-                Log.e("WifiManager", "Erro ao conectar: ${e.message}")
+                Log.e("WifiManager", "Error while connecting: ${e.message}")
             }
         }
     }
@@ -95,36 +89,34 @@ class WifiManager(private val context: Context) {
             outputStream = socket?.getOutputStream()
             inputStream = BufferedReader(InputStreamReader(socket?.getInputStream()))
         } catch (e: IOException) {
-            Log.e("WifiManager", "Erro ao configurar streams: ${e.message}")
+            Log.e("WifiManager", "Error setting up streams: ${e.message}")
         }
     }
 
     fun sendData(message: String) {
-        Log.d("WifiManager", "Mensagem a enviar: $message")
-        // Verifica se a conexão está ativa
+        Log.d("WifiManager", "Message to send: $message")
         listenForMessages()
         if (socket != null && socket!!.isConnected && outputStream != null) {
             executor.execute {
                 try {
-                    Log.d("WifiManager", "Tentando enviar mensagem: $message")
+                    Log.d("WifiManager", "Trying to send message : $message")
                     outputStream?.write((message + "\n").toByteArray())
-                    Log.d("WifiManager", "Mensagem enviada: $message")
+                    Log.d("WifiManager", "Message sent: $message")
                     outputStream?.flush()
-                    Log.d("WifiManager", "Mensagem enviada com sucesso.")
+                    Log.d("WifiManager", "Message succesfuly sent.")
                 } catch (e: IOException) {
-                    Log.e("WifiManager", "Erro ao enviar mensagem: ${e.message}")
-                    // Tente reconectar ou tratar o erro aqui
+                    Log.e("WifiManager", "Error while trying to send message: ${e.message}")
                     handleBrokenPipe()
                 }
             }
         } else {
-            Log.e("WifiManager", "Erro: socket ou outputStream não está disponível.")
+            Log.e("WifiManager", "Error: socket or outputStream not available.")
         }
     }
 
     fun handleBrokenPipe() {
         // Aqui, você pode tentar reconectar ou fechar o socket corretamente
-        Log.e("WifiManager", "Tentando reconectar...")
+        Log.e("WifiManager", "Trying to reconnect...")
         reconnect()
     }
 
